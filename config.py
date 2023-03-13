94c790a52f35e1656651c644c76b88b04c28c091
T bone <66728271+toblaroni@users.noreply.github.com>
2023-03-13 17:40:38 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'a-very-secret-secret'
+SECRET_KEY = '###'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
